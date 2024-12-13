59b124fac02b664fd2bf661314a1884d5b16262c
SAMShonap <joserondan8@gmail.com>
2024-12-13 18:12:10 -0500

    Primera versión del proyecto un poco mejorado

--- js/app.js
+++ js/app.js
@@ -1,39 +1,32 @@
-// Variables
+
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
-let articulosCarrito = [];
 
-// Listeners
 cargarEventListeners();
 
 function cargarEventListeners() {
-     // Dispara cuando se presiona "Agregar Carrito"
+
      listaCursos.addEventListener('click', agregarCurso);
 
-     // Cuando se elimina un curso del carrito
      carrito.addEventListener('click', eliminarCurso);
 
-     // Al Vaciar el carrito
+
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
 }
 
-
-// Funciones
-// Función que añade el curso al carrito
 function agregarCurso(e) {
     e.preventDefault();
-    // Delegation para agregar-carrito
+
     if(e.target.classList.contains('agregar-carrito')) {
          const curso = e.target.parentElement.parentElement;
-         // Enviamos el curso seleccionado para tomar sus datos
+
          leerDatosCurso(curso);
     }
 }
 
-// Lee los datos del curso
 function leerDatosCurso(curso) {
     const infoCurso = {
          imagen: curso.querySelector('img').src,
@@ -58,30 +51,23 @@ function leerDatosCurso(curso) {
          articulosCarrito = [...articulosCarrito, infoCurso];
     }
 
-    // console.log(articulosCarrito)
-
-    
-
-    // console.log(articulosCarrito)
+  
     carritoHTML();
 }
 
-// Elimina el curso del carrito en el DOM
+
 function eliminarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('borrar-curso') ) {
-         // e.target.parentElement.parentElement.remove();
+
          const cursoId = e.target.getAttribute('data-id')
-         
-         // Eliminar del arreglo del carrito
+
          articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
 
          carritoHTML();
     }
 }
 
-
-// Muestra el curso seleccionado en el Carrito
 function carritoHTML() {
 
     vaciarCarrito();
@@ -104,13 +90,8 @@ function carritoHTML() {
 
 }
 
-// Elimina los cursos del carrito en el DOM
 function vaciarCarrito() {
-    // forma lenta
-    // contenedorCarrito.innerHTML = '';
 
-
-    // forma rapida (recomendada)
     while(contenedorCarrito.firstChild) {
          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
      }
